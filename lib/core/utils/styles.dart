76f7cc9f3500e72ee608917e87a51460c5ab503b
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2023-10-04 19:57:41 +0300

    Utils

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class Style
+{
+  static const style20= TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const style14= TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const style12= TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+}
